832ce17306fd7d9ea0f9bd09b08225ca7b786a9b
watanabe <kouki.1101@outlook.jp>
2022-05-05 22:21:00 +0900

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,40 @@
+pub trait SetMinMax {
+    fn setmin(&mut self, v: Self) -> bool;
+    fn setmax(&mut self, v: Self) -> bool;
+}
+impl<T> SetMinMax for T
+where
+    T: PartialOrd,
+{
+    fn setmin(&mut self, v: T) -> bool {
+        *self > v && {
+            *self = v;
+            true
+        }
+    }
+    fn setmax(&mut self, v: T) -> bool {
+        *self < v && {
+            *self = v;
+            true
+        }
+    }
+}
+
+/// 時間計測開始時に一度呼び出し、その後呼ぶと経過時間が秒で返ってくる。
+/// const TL: f64 = 1.95;
+/// loop {
+///     if get_time()/TL >= 1.0 {
+///         break;
+pub fn get_time() -> f64 {
+    static mut STIME: f64 = -1.0;
+    let t = std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)
+        .unwrap();
+    let sec = t.as_secs() as f64 + t.subsec_nanos() as f64 * 1e-9;
+    unsafe {
+        if STIME < 0.0 {
+            STIME = sec;
+        }
+        sec - STIME
+    }
+}
