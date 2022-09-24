450c32af5767c0ffc1d6ee1d1b36d37168661a09
watanabe <kouki.1101@outlook.jp>
2022-09-25 05:20:10 +0900

    リセット機能追加

--- src/lib.rs
+++ src/lib.rs
@@ -43,18 +43,19 @@ where
 }
 
 /// 時間計測開始時に一度呼び出し、その後呼ぶと経過時間が秒で返ってくる。
+/// 引数にtrueを入れると計測リセット（複数テストケース用）
 /// const TL: f64 = 1.95;
 /// loop {
-///     if get_time()/TL >= 1.0 {
+///     if get_time(false)/TL >= 1.0 {
 ///         break;
-pub fn get_time() -> f64 {
+pub fn get_time(reset_f: bool) -> f64 {
     static mut STIME: f64 = -1.0;
     let t = std::time::SystemTime::now()
         .duration_since(std::time::UNIX_EPOCH)
         .unwrap();
     let sec = t.as_secs() as f64 + t.subsec_nanos() as f64 * 1e-9;
     unsafe {
-        if STIME < 0.0 {
+        if STIME < 0.0 || reset_f {
             STIME = sec;
         }
         sec - STIME
